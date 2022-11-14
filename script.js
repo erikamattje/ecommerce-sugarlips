9b0df2f28a5c4556cb56de8b71cf6a349c588eb9
Erika Mattje <erikamattje@gmail.com>
2022-11-13 22:59:13 -0300

    :package: Funcionalidade Adicionar ao carrinho

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 const items = [
     {
         id: 0,
-        nome: 'Kit 1',
+        nome: 'Rímel Big Bang',
         img: './images/produto1.jpg',
         quantidade: 0
     },
     {
         id: 1,
-        nome: 'Kit 2',
+        nome: 'Batom Líquido Finérrimo',
         img: './images/produto3.jpg',
         quantidade: 0
     },
     {
         id: 2,
-        nome: 'Kit 3',
+        nome: 'Creme Nutri',
         img: './images/produto5.jpg',
         quantidade: 0
     },
 
     {
         id: 3,
-        nome: 'Kit 4',
+        nome: 'Liptint Finérrimo',
         img: './images/produto4.jpg',
         quantidade: 0
     },
     {
         id: 4,
-        nome: 'Kit 5',
+        nome: 'Delineador + Demaquiliante',
         img: './images/produto6.jpg',
         quantidade: 0
     },
     {
         id: 5,
-        nome: 'Kit 6',
+        nome: 'Kit Skin Care',
         img: './images/produto7.jpg',
         quantidade: 0
     },
@@ -47,7 +47,7 @@ inicializarLoja = () => {
         <div class="produto-single">
             <img src="`+val.img+`" />
             <p> `+val.nome+` </p>
-            <a key="`+val.id+`" href="#"> Adicionar ao carrinho!</a>
+            <a key="`+val.id+`"><button class="button">Adicionar ao carrinho</button><a/>
         </div>
 
         `;
@@ -57,13 +57,26 @@ inicializarLoja = () => {
 
 inicializarLoja();
 
+
 atualizarCarrinho = () => {
-    console.log(items);
+    var containerCarrinho = document.getElementById('carrinho');
+    containerCarrinho.innerHTML = "";
+    items.map((val)=> {
+        if (val.quantidade > 0) {
+            containerCarrinho.innerHTML += `
+            
+            <p> `+val.nome+` | Quantidade: `+val.quantidade+`</p>
+            <hr>
+            `;
+        }
+                    
+        
+    })
 }
 
 var links = document.getElementsByTagName('a');
 
-for(var i=0; i<links.length; i++){
+for(var i=0; i< links.length; i++){
     links[i].addEventListener("click", function(){
         let key = this.getAttribute('key');
         items[key].quantidade++;
